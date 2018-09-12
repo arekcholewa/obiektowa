3d48171ff5cf751da5319938924fba1831bff611
Arek <arkadiusz.cholewa.gm@gmail.com>
2018-09-12 23:58:08 +0200

    start repo

---
+++ DataModel.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using Starcounter;
+
+namespace StarcounterApplication1
+{
+    class DataModel
+    {
+        public void createData()
+        {
+            var company1 = Db.Transact(() => new Company()
+            {
+                name = "MyCompany"
+            });
+
+            var departmentFinanse = Db.Transact(() => new Department()
+            {
+                name = "Finanse",
+                Company = company1
+            });
+
+            var departmentIt = Db.Transact(() => new Department()
+            {
+                name = "IT",
+                Company = company1
+            });
+
+      
+            var positionManager = Db.Transact(() => new Position()
+            {
+                name = "Manager",
+                salary = 4500
+            });
+
+            var positionKsiegowy = Db.Transact(() => new Position()
+            {
+                name = "Ksiegowy",
+                salary = 5000
+            });
+
+            var positionAdministrator = Db.Transact(() => new Position()
+            {
+                name = "Administrator",
+                salary = 6000
+            });
+
+
+            var adres1 = Db.Transact(() => new Address()
+            {
+                country = "Polska",
+                buldingNo = 124,
+                Street = "Kasprowicza"
+            });
+
+            var adres2 = Db.Transact(() => new Address()
+            {
+                country = "Polska",
+                buldingNo = 12,
+                Street = "Kasprzaka"
+            });
+
+            var emp1 = Db.Transact(() => new Employee()
+            {
+                FirstName = "Krzysztof",
+                LastName = "Kowalski",
+                Department = departmentFinanse,
+                position = positionKsiegowy,
+                adress = adres1
+
+
+            });
+            var emp2 = Db.Transact(() => new Employee()
+            {
+                FirstName = "Jan",
+                LastName = "Malinowski",
+                Department = departmentFinanse,
+                position = positionManager,
+                adress = adres2
+
+            });
+
+            var emp3 = Db.Transact(() => new Employee()
+            {
+                FirstName = "Wojciech",
+                LastName = "Adamski",
+                Department = departmentIt,
+                position = positionManager,
+                adress = adres2
+
+            });
+
+            var emp4 = Db.Transact(() => new Employee()
+            {
+                FirstName = "Adam",
+                LastName = "Abacki",
+                Department = departmentIt,
+                position = positionAdministrator,
+                adress = adres2
+
+            });
+
+            var emp5 = Db.Transact(() => new Employee()
+            {
+                FirstName = "Michal",
+                LastName = "Kochanowski",
+                Department = departmentIt,
+                position = positionManager,
+                adress = adres1
+
+            });
+
+        }
+
+        public void deleteData()
+        {
+            //DELETE FROM StarCounterApplication1.Employee
+           //Delete  FROM  StarCounterApplication1.Position
+           // DELETE FROM StarCounterApplication1.Address
+            // DELETE FROM StarCounterApplication1.Department
+            //DELETE FROM StarCounterApplication1.Company
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using Starcounter;
+using Starcounter.Internal;
+
+namespace StarcounterApplication1
+{
+    
+   
+    [Database]
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName { get { return FirstName + " " + LastName; } }
+    }
+
+    [Database]
+    public class Employee : Person
+    {
+      
+        public Department Department { get; set;  }
+        public Address adress;
+        public Position position { get; set; }
+
+        public String introduce()
+        {
+            return LastName + " " + FirstName;
+        }
+
+        public void setPosition(Position position)
+        {
+            Db.Transact(() => this.position = position);
+        }
+
+    }
+
+    [Database]
+    public class Department 
+    {
+        public String name;
+        public Company Company { get; set; }
+        IEnumerable Employees  =>  Db.SQL<Employee>(
+        "SELECT e FROM Employee e WHERE e.Department = ?", this);
+
+
+        public double countSalary()
+        {
+            double sum = 0;
+            foreach (var empl in Employees)
+            {
+                Employee e = (Employee) empl;
+                sum += e.position.salary;
+            }
+            return sum;
+        }
+    }
+    [Database]
+    public class Company
+    {
+        public String name;
+        public IEnumerable Department =>
+        Db.SQL<Department>(
+        "SELECT e FROM Employee e WHERE e.Company = ?", this);
+       
+
+    }
+
+    [Database]
+    public class Address
+    {
+        public String country;
+        public int buldingNo;
+        public String Street;
+
+    }
+
+    [Database]
+    public class Position
+    {
+        public String name;
+        public int salary;
+        public IEnumerable Employees =>  Db.SQL<Employee>(  "SELECT e FROM Employee e WHERE e.Position = ?", this);
+
+        public void setSalary(int salary)
+        {
+            Db.Transact(() => this.salary = salary);
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            // new DataModel().createData();
+            
+            
+           var department = Db.SQL<Department>("SELECT d FROM Department d Where d.name = ?", "Finanse").First();
+            Console.Write("Suma pensji dla departamentu: "+department.countSalary());
+
+            var manager = Db.SQL<Position>("SELECT p FROM StarcounterApplication1.Position p where name = ? ", "Manager").First();
+           manager.setSalary(6700);
+
+            var employee = Db.SQL<Employee>("SELECT e FROM Employee e Where e.FirstName = ?", "Krzysztof").First();
+            employee.setPosition(manager);
+            
+        }
+    }
+}
